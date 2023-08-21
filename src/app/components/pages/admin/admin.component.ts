8b7de20793cdf9d0bcbfdd3f1dba56b960335053
Sebastián <seba.chavez21@gmail.com>
2023-08-21 17:28:54 -0400

    cambio diseño

--- src/app/components/pages/admin/admin.component.ts
+++ src/app/components/pages/admin/admin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { MenuService } from '../../../services/menu/menu.service';
 import { LoggerService } from 'src/app/services/logger/logger.service';
 
@@ -11,12 +11,30 @@ export class AdminComponent implements OnInit {
 
   idLog: string = 'AdminComponent'
   stateSidemenu: boolean = false;
+  width = window.innerWidth
+  isResponsive: boolean = false
+
   constructor(
     private menuService: MenuService,
     private logger: LoggerService
   ) { }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    console.log(event.target.innerWidth);
+    if(event.target.innerWidth < 767){
+      this.isResponsive = true
+    } else {
+      this.isResponsive = false
+    }
+  }
+
   ngOnInit(): void {
+    if(this.width < 767){
+      this.isResponsive = true
+    } else {
+      this.isResponsive = false
+    }
     this.menuService.getStateSidemenu().subscribe((res: boolean) => {
       this.stateSidemenu = res
     })
